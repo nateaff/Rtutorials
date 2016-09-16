83875d39de77c7215a8efdc7c93985030aa9a512
Nathanael Aff <nathanaelaff@gmail.com>
2016-09-16 09:00:11 -0700

    tut2 update

--- docs/tut1.R
+++ docs/tut1.R
@@ -1,7 +1,6 @@
 
 # Introduction to R : Tutorial 1 (9/)
 
-
 # where you are
 getwd()
 ?setwd()
@@ -9,9 +8,10 @@ dir()
 
 
 # arithmetic
-  5 + 12
-  20 * 3 - 5/2
-  4^2
+5 + 12
+20 * 3 - 5/2
+4^2
+
 
 # assignment
 x <- 13*2
@@ -31,7 +31,6 @@ x + y
 # get help
 ?"%%"
 
-
 # **Exercise**
 # What does the "%/%" operator do? Use the help or experiment. 
 
@@ -44,131 +43,126 @@ x + y
 # 4. Integer
 # 5. Complex
 
-  5.5             # double
-  TRUE == FALSE   # logical
-  5 == 2          # returns a logical
-  "a"             # character
-  "hello"         
-  2L              # integer
- 
+5.5             # double
+TRUE == FALSE   # logical
+5 == 2          # returns a logical
+"a"             # character
+"hello"         
+2L              # integer
+
 
 ## Vectors
 
-  x <- 4.4
-  is.vector(x)
+x <- 4.4@
+is.vector(x)
 
-  # double
-  (vec <- c(0,1,2,3,4))
-  length(vec)
-  mode(vec)
-  is.numeric(vec)s
-  is.double(vec)
+# double
+(vec <- c(0,1,2,3,4))
+vec <- c(0,1,2); vec
 
-  # generate seqeunce
-  1:6
+length(vec)
+mode(vec)
+is.numeric(vec)s
+is.double(vec)
 
-  # character
-  char_vec <- c("a", "b", "c")
-  mode(char_vec)
-  length(char_vec)
-  is.character(char_vec)
+# generate seqeunce
+1:6
 
-  # integer type
-  int_vec <- c(1L, 2L, 3L)
-  is.numeric(int_vec)
-  levels(int_vec)
+# character
+char_vec <- c("a", "b", "c")
+mode(char_vec)
+length(char_vec)
+is.character(char_vec)
 
-  # boolean or logical 
-  bool_vec <- c(TRUE, FALSE, TRUE, FALSE)
-  is.logical(bool_vec)
+# integer type
+int_vec <- c(1L, 2L, 3L)
+is.numeric(int_vec)
+levels(int_vec)
 
-
+# boolean or logical 
+bool_vec <- c(TRUE, FALSE, TRUE, FALSE)
+is.logical(bool_vec)
 
 # concatenation
-  vec2 <- c(1,2,3, int_vec)
+vec2 <- c(1,2,3, int_vec)
 
 # create a vectors of length 10 with default values
-  character(10)
-  logical(10)
+character(10)
+logical(10)
+double(10)
 
-  double(10)
+# an alternative method 
+x <- vector(mode = "double", length = 10)
 
-  # an alternative method 
-  x <- vector(mode = "double", length = 10)
-
-  # this works the same as the previous command
-  x <- vector("double", 10)
-
+# this works the same as the previous command
+x <- vector("double", 10)
 
 ## Factors
 
-  # create factors from characters
-  factor_vec <- factor(c("a", "b", "c"))
-  levels(factor_vec)
+# create factors from characters
+factor_vec <- factor(c("a", "b",c "c"))
+levels(factor_vec)
 
-  # coerce a character vector to a factor
-  name_vec <- c("Sue", "Saghi", "Eileen", "Fred", "Fred", "Sue")
+# coerce a character vector to a factor
+name_vec <- c("Sue", "Saghi", "Eileen", "Fred", "Fred", "Sue")
 
-  # show unique names: 
-  unique(name_vec)
-  (name_factors <- as.factor(name_vec))
+# show unique names: 
+unique(name_vec)
+(name_factors <- as.factor(name_vec))
 
-  #access levels
-  levels(name_factors)
-  levels(name_factors)[2]
+#access levels
+levels(name_factors)
+levels(name_factors)[2]
 
-  # a common way to create a factor vector
-  rep(c("Sue", "Saghi", "Eileen"), 3)
-  rep(c("Sue", "Saghi", "Eileen"), each = 3)
+# a common way to create a factor vector
+rep(c("Sue", "Saghi", "Eileen"), 3)
+rep(c("Sue", "Saghi", "Eileen"), each = 3)
 
-  # combining the operations
-  factor(rep(c("Sue", "Saghi", "Eileen"), 3))
+# combining the operations
+factor(rep(c("Sue", "Saghi", "Eileen"), 3))
 
 
 
 ### Special values 
 
-  na_vec <- c(1,2,3,4, NA)
-  is.na(na_vec)
+na_vec <- c(1,2,3,4, NA)
+is.na(na_vec)
 
-  sum(na_vec)
-  # sum after omitting na values
-  sum(omit.na(na_vec))
+sum(na_vec)
+# sum after omitting na values
+sum(omit.na(na_vec))
 
-  # sum a subset
-  sum(na_vec[1:4])
+# sum a subset
+sum(na_vec[1:4])
 
 
 ## Operations with `NA`'s will return `NA`.
 
-
-  1 + NA 
-  2* NA
-
-
-  2 + Inf
-  2/Inf 
-  Inf - Inf
-
-  is.null(NULL)
+1 + NA 
+2* NA
 
 
+2 + Inf
+2/Inf 
+Inf - Inf
+
+is.null(NULL)
 
 ### Subset an atomic vector
 
-  # letters is a built-in R variable
-  letters[1:20]
-  abcs <- Letters[1:5]
-  abcs[1:2]
-  # omit the third element
-  abcs[-3]
-  # select based on a vector
-  abcs[c(1,3,5)]
+# letters is a built-in R variable
+letters[1:20]
+abcs <- LETTERS[1:5]
+abcs[1:2]
+# omit the third element
+abcs[-3]
+# select based on a vector
+abcs[c(1,3,5)]
 
 ## logical vector subsetting
 
 # set a seed for reproducible random number generation
-set. seed(1)
+set.seed(1)
 
 numbers <- rnorm(10, mean=0, sd =10)
 # returns logical vector
@@ -184,150 +178,165 @@ numbers["a"]
 
 # A few examples of useful functions:
 
-  median(vec)
-  mean(vec)
-  sum(vec)
-  var(vec)
-  sd(vec)
-  max(3:10)
-  min(5:10)
-  range(1:20)
-  quantile(1:20)
-  summary(1:20)
+median(vec)
+mean(vec)
+sum(vec)
+var(vec)
+sd(vec)
+max(3:10)
+min(5:10)
+range(1:20)
+quantile(1:20)
+summary(1:20)
 
 
 # --- Lists
 
-  # create list
-  mylist <- list(numbers  = rnorm(10, 4, 3), letters[1:20])
-  names(mylist)
+# create list
+mylist <- list(numbers  = rnorm(10, 4, 3), letters[1:20])
+names(mylist)
 
-  # change name of second element
-  names(mylist)[2] <- "letters"
-  names(mylist)
+# change name of second element
+names(mylist)[2] <- "letters"
+names(mylist)
 
 # list subsetting
 
-  mylist$numbers
-  list1 <- mylist[1]
-  is.vector(list1)
+mylist$numbers
+list1 <- mylist[1]
+is.vector(list1)
 
-  vec1 <- mylist[[2]]
-  is.vector(vec1)
+vec1 <- mylist[[2]]
+is.vector(vec1)
 
 
 
 #--- Matrices 
 
-  matrix(c(1, 2, 3, 4))
-  matrix(1:4, nrow = 2, ncol = 2)
+matrix(c(1, 2, 3, 4))
 
-  M <- matrix(rnorm(16), ncol= 4, ncol = 4, byrow = TRUE)
+matrix(1:4, nrow = 2, ncol = 2)
+matrix(1:4, nrow = 2, ncol = 2, byrow = TRUE)
 
+
+M <- matrix(rnorm(16), ncol= 4, nrow = 4, byrow = TRUE)
+M
 # Operations
-  M %*% M
-  M*M
-  M^2
-  solve(M)
+M %*% M
 
-  dim(M)
+M*M
+M^2
 
-  # access an element
-  M[1,2]
-  # access a row
-  M[,1]
+solve(M)
+
+dim(M)
+
+# access an element
+M[1,2]
+
+# access a row
+M[,1]
 
 
 # Retrieving a column of a matrix returns a vector, not a matrix.
 
-  is.matrix(M[,1])
-  is.vector(M[,1])
+is.matrix(M[,1])
+is.vector(M[,1])
+
+
 
 # --- data frame basics
-  # some vectors of the same length
-  abc <- LETTERS[1:3]
-  nums <- rpois(3, 10)
-  facs <- c("red", "green", "blue")
+# some vectors of the same length
+abc <- LETTERS[1:3]
+nums <- rpois(3, 10)
+facs <- factor(c("red", "green", "blue"))
 
-  # create a data frame
-  df <- data.frame(abc, nums, facs)
+# create a data frame
+df <- data.frame(abc, nums, facs)
 
-  # add a column 
-  df$ints <-  3:5
+# add a column 
+df$ints <-  3:5
 
-  # summaries of data 
-  summary(df)
-  head(df)
-  names(df)
+# summaries of data 
+summary(df)
+head(df)
+names(df)
 
-  #replace names
-  names(df) <- c("letters", "numbers", "factors", "integers")
-  df
-  row.names(df)
+#replace names
+names(df) <- c("letters", "numbers", "factors", "integers")
+df
+row.names(df)
 
-  # subsetting
-  df[1,2]
+# subsetting
+df[1,2]
 
-  df[1]
-  is.vector(df[1])
-  is.vector(df[[1]])
+df[1]
+is.vector(df[1])
+is.vector(df[[1]])
 
-  is.list(df)
-  is.data.frame(df)
-  mean(df[,2])
-  sum(df[,2])
+is.list(df)
+is.data.frame(df)
+mean(df[,2])
+sum(df[,2])
 
-  # add a column 
-  df$ints <-  3:5
+# add a column 
+df$ints <-  3:5
+
 
 
 # --- simulate a die role
-  seed(1)
-  die <- 1:6
-  # this does not work
-  sample1 <- sample(die, 1
+set.seed(1)
+die <- 1:6
+# this does not work
+sample1 <- sample(die, 1)
+
 
 # --- plot example
-  plot(sample1)
+plot(sample1)
 
-  # save defaults
-  oldpar <- par  
-  # set number of rows, columns with mfrow
-  par(mfrow = c(1,3))
-  hist(sample1)
-  hist(sample1)
-  hist(sample1)
-  # restore defaults
-  par <- oldpar
 
-  n1 <- rnorm(10000, mean = 3, sd = 1.5)
-  n2 <- rnorm(10000, mean = -4, sd = 2)
-  n3 <- rnorm(10000, mean = 2, sd = 5)
+# save defaults
+oldpar <- par  
+# set number of rows, columns with mfrow
+par(mfrow = c(1,3))
 
-  hist(c(n1,n2,n3), col = "gray30", border = "white")
+hist(rolls)
+hist(sample1)
+hist(sample1)
+# restore defaults
+par <- oldpar
 
-  p1 <- hist(n1)
-  p2 <- hist(n2)
-  p3 <- hist(n3)
 
-  d1 <- density(rnorm(10000, mean = 3, sd = 1.5))
-  d2 <- density(rnorm(10000, mean = -4, sd = 2))
-  d3 <- density(rnorm(10000, mean = 2, sd = 5))
 
-  plot( p1, col=rgb(0,0,1,1/8), xlim=c(-20, 20), xlab = "",
-  main = "Mixture of Gaussians" )
-  plot( p2, col=rgb(1, 0, 0,1/8), xlim=c(-20,20), xlab = "", add=TRUE) 
-  plot( p3, col=rgb(0, .7, 0, 1/8), xlim=c(-20,20), xlab ="", add=TRUE)  
 
-  # a simple plot function
+n1 <- rnorm(10000, mean = 3, sd = 1.5)
+n2 <- rnorm(10000, mean = -4, sd = 2)
+n3 <- rnorm(10000, mean = 2, sd = 5)
 
-  add_density <- function(dens, color){
-    par(new = TRUE)
-    plot(dens, col = color, xlim = c(-20, 20), ylab ="", xlab="",main="", yaxt = "n", xaxt= "n")  
-  }
+hist(c(n1,n2,n3), col = "gray30", border = "white")
 
-  add_density(d1, "blue")
-  add_density(d2, "red")
-  add_density(d3, "green")
+p1 <- hist(n1)
+p2 <- hist(n2)
+p3 <- hist(n3)
+
+d1 <- density(rnorm(10000, mean = 3, sd = 1.5))
+d2 <- density(rnorm(10000, mean = -4, sd = 2))
+d3 <- density(rnorm(10000, mean = 2, sd = 5))
+
+plot( p1, col="green", xlim=c(-20, 20), xlab = "",
+main = "Mixture of Gaussians" )
+plot( p2, col=rgb(1, 0, 0, 1/8), xlim=c(-20,20), xlab = "", add=TRUE) 
+plot( p3, col=rgb(0, .7, 0, 1/8), xlim=c(-20,20), xlab ="", add=TRUE)  
+
+# a simple plot function
+
+add_density <- function(dens, color){
+  par(new = TRUE)
+  plot(dens, col = color, xlim = c(-20, 20), ylab ="", xlab="",main="", yaxt = "n", xaxt= "n")  
+}
+
+add_density(d1, "blue")
+add_density(d2, "red")
+add_density(d3, "green")
 
 
--- docs/tut2.md
+++ docs/tut2.md
@@ -1,15 +1,68 @@
-
+ 
+ 
 
 # Control structures and Functions
 
 This tutorial will cover basic control structures in R, writing functions and some the `apply` family of functions.
 I assume some basic familiarity with programming such as a first class in programming.
 
+
+## A simple function
+
+We'll start off with the structure of a basic function and then build on that once we've gone over some control structures. 
+
+Here is the basic syntax of a function: 
+
+```
+fname <- function(arg1, arg2, arg3 = default.value, etc.){
+  function.body
+}
+
+```
+
+The function defined in the function body will be assigned to the variable `fname`. The arguments : `arg1, arg2, arg3` are the values passed to the function and available to operations in the function body.
+
+
+Here's a simple example:
+
+```
+echo <- function(arg ){ 
+  cat(arg, "\n")
+}
+
+echo("arrrrrrrg")
+
+```
+
+You can define a function on a single line without brackets, but for now I'd recommend using brackets to define the function body. 
+
+The `cat` function writes its argument to the console and it can take a combination of strings and variables. (The `"\n"` is equivalent to hitting return after writing a sentence).
+
+Here's another simple example: 
+```{r}
+add <- function(a,b){
+  a + b
+}
+
+add(3,3)
+
+# with an explicit return function
+
+add2 <- function(a,b){
+  res <- a + b
+  return(res)
+}
+
+add2(2,3)
+
+```
+
+
 ## Control structures
 
 Control structures determine the flow of execution in a program. The most basic example of controlling program flow is branch on some condition. 
 
-```
+```{r}
   x = 5
 
   if(x == 4) {
@@ -79,12 +132,239 @@ while(k <= 1024){
 
 ```
 
-## Vectorized function
+There is another way to create for loops that can be safer then the `for` syntax mentioned above. 
+
+
+```
+# seq_along
+
+# this behavior is unexpected for a zero length vector
+xx <- NULL
+for(k in 1:(length(xx))){
+  cat("k: ", k, "\n")
+}
+
+for(k in seq_along(xx)){
+  cat("k: ", k, "\n")
+}
+
+
+xx <- 3:20
+length(xx)
+new_vec <- double(length(xx))
+for(k in seq_along(xx)){
+  cat("k: ", k, "\n")
+  new_vec[k] <- k + 5
+}
+new_vec
+
+```
+The `seq_along()` function can be used to iterate over structures that have some length or dimension.
+
+
+## `apply` type functions
+
+First we'll introduce the replicate function. 
+
+Here's the usage from the documentation:
+
+```
+replicate(n, expr, simplify = "array")
+
+``` 
+The `expr` is some expression that is evaluated `n` times. Here I use it to create a data frame.
+
+```
+expr1 <- rnorm(10, 0, 1)
+
+df <- data.frame(replicate(5, expr1))
+df
+# we can't do this
+mean(df)
+
+# we can do this
+apply(df, 2, mean)
+apply(df, 1, mean, trim = 0.1)
+
+# using a matrix
+M <- as.matrix(df)
+apply(M, 2, sd)
+```
+
+Here is the function usage 
+`apply(X, MARGIN, FUN, ...)` 
+
+`X` is an array structure, a data frame or a matrix, for example.  The margin is the (1 for rows and 2 for columns), and FUN is the function you want to apply to the columns.
+
+The `...` means additional arguments can be passed the function. In the above example `trim = 0.1` is an argument to the mean function. 
+
+There is a family of apply style functions. The `lapply` function operates on lists, `tapply` can operate on a vector where operations are grouped based on second index argument.
+
+
+Here's a R version of `lapply` from [Advanced R](http://adv-r.had.co.nz/Functionals.html) which gives the idea of what the function does under the hood.
+
+```
+lapply2 <- function(x, f, ...) {
+  out <- vector("list", length(x))
+  for (i in seq_along(x)) {
+    out[[i]] <- f(x[[i]], ...)
+  }
+  out
+}
+
+```
+
+Here's an example of operating on a list: 
+
+```
+# A list of vectors of different size
+vec_list <- list(a = c(1:10, NA), b = 2:20, c = 3:40, d = 5:50)
+
+lapply(vec_list, sum)
+lapply(vec_list, mean)
+
+# remove NA's before computing means
+lapply(vec_list, mean, na.rm = TRUE)
+lapply(vec_list, sd, na.rm = TRUE )
+
+res <- lapply(vec_list, sd, na.rm = TRUE )
+unlist(res)
+```
+
+The `lapply` function returns a list of the length of the list passed to the function. The `unlist` function can be used to create a vector of the results. 
+
+User defined functions can also be used. In addition, you can define _anonymous functions_ that are defined with the lapply argument. Here is the syntax:
+
+```
+# anonymous functions
+(function(x) x*2)(2)
+lapply(vec_list, function(x) x*2)
+
+
+``` 
+
+
+
+```
+  # paste/paste0 functions
+  paste0("filename", 1, ".csv")
+
+  # create a list of file names
+  dflist <- lapply(2:10, function(x) paste0("filename", x, ".csv"))
+  dflist
+
+  # seq
+  seq(2, 20, by = 4)
+  nums <- seq(2, 20, by = 4)
+
+  # repeating above with a different sequence
+  dflist <- lapply(nums , function(x) paste0("filename", x, ".csv"))
+  dflist
+
+```
+
+There are also 
 
 ## Functions
 
-## apply functions
+We've already seen the basic form of a function. Here we add a few more details and create some more complex functions. 
+
+Many of the base R functions have arguments with default values. You can also add default values to your functions.
+
+```
+# default arguments
+def0 <- function(a, b = 1){
+  a^b
+}
+
+def0(1,2)
+def0(2)
+
+# can change order if you name the argument
+def(b=1, 2)
+
+```
+The arguments will be evaluated based on their position unless they are named. 
+
+If a variable isn't defined in a function R will look the next level up based on the environment in which the function was created.
+
+```
+  x <- 100
+  f <- function( ) {
+    y <- 2
+    c(x, y)
+  }
+  f()
+  rm(f)
+
+```
+
+For loops are known for being slow in R and you can often rewrite a for loop using a vectorized function using a `apply` type function. 
+
+```
+
+  fill_NAs <- function(v){
+    
+    ind <- is.na(v)
+    mu <- mean(v, na.rm = TRUE, trim = 0.1)
+    v[ind] <- mu
+    return(v)
+  }
+
+  vec <- c(1:5, rep(NA, 5), 3:10)
+  fill_NAS(vec)
+
+```
 
 
-Dataframe
+We'll take an example 
+[from here](https://www.countbayesie.com/blog/2015/3/3/6-amazing-trick-with-monte-carlo-simulations) of running a simple simulation to test of the difference of two observations are significant. 
 
+Here's the setup : 
+> Let's suppose we're comparing two webpages to see which one converts our customers to "sign up" at a higher rate (This is commonly referred to as an A/B Test). For page A we have seen 20 convert and 100 not convert, for page B we have 38 converting and 110 not converting. We'll model this as two Beta distributions as we can see below:
+
+```
+# generate samples from the distributions
+runs <- 100
+rbeta(runs, 38, 110)
+rbeta(runs, 20, 100)
+
+# vary the number of runs and compute the percent of winners
+
+sig_test <- function(runs){
+  a <- rbeta(runs, 38, 110)
+  b <- rbeta(runs, 20, 100)
+  p_val <- 1- sum(a > b)/runs
+  return(p_val)
+}
+
+# check time
+system.time(pval1 <- sig.test(1000))
+system.time(pval2 <- sig.test(100000))
+
+```
+
+## Closures
+
+You can also create functions that return functions based on input. 
+
+```
+make_pow <- function(n){
+  function(a){
+    a^n
+  }
+}
+
+pow3 <- make_pow(3)
+pow4 <- make_pow(4)
+is.function(pow3)
+
+pow3(2)
+pow4(3)
+
+# create a list of functions
+pows <- lapply(5:10, make_pow)
+# pass the functions to the anonymous function to evaluate
+lapply(pows, function(x) x(2:4))
+
+``` 
